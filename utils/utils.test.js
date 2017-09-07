81448caae21b6b8f52aa94d879f77c7edbd06bef
fatihmert-medyanet <fatih.mert@medyanet.com.tr>
2017-09-07 16:44:05 +0300

    test_code

---
+++ utils/utils.test.js
@@ -0,0 +1,12 @@
+const utils = require('./utils')
+const expect = require("expect");
+
+it('should add two numbers', () => {
+    var res = utils.add(33,11);
+    expect(res).toBe(44);
+});
+
+it('should square a number', () => {
+    var res = utils.square(3);
+    expect(res).toBe(9);
+});
